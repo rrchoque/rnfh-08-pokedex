5a681b0e7e8a26b51c4665702be418c33f2fa026
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-05-09 08:48:04 -0400

    se configuró React Navigation y se creó la estructura de directorios inicial

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import {name as appName} from './app.json';
+import { PokedexApp } from './src/presentation/PokedexApp';
+
+AppRegistry.registerComponent(appName, () => PokedexApp);
---
+++ src/presentation/PokedexApp.tsx
@@ -0,0 +1,12 @@
+import 'react-native-gesture-handler';
+
+import { NavigationContainer } from "@react-navigation/native";
+import { StackNavigator } from './navigator/StackNavigator';
+
+export const PokedexApp = () => {
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
+};
