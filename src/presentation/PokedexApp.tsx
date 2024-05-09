d09df6cbfdebb5091b7f0d94fa3bd20406526ffa
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-05-09 18:25:47 -0400

    Se instaló React Native Papper

--- src/presentation/PokedexApp.tsx
+++ src/presentation/PokedexApp.tsx
@@ -2,11 +2,15 @@ import 'react-native-gesture-handler';
 
 import { NavigationContainer } from "@react-navigation/native";
 import { StackNavigator } from './navigator/StackNavigator';
+import { PaperProvider } from 'react-native-paper';
 
 export const PokedexApp = () => {
   return (
-    <NavigationContainer>
-      <StackNavigator />
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <StackNavigator />
+      </NavigationContainer>
+    </PaperProvider>
+
   );
 };
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,9 +1,14 @@
 import { View, Text } from 'react-native'
+import { Button } from 'react-native-paper'
 
 export const HomeScreen = () => {
   return (
     <View>
       <Text>HomeScreen</Text>
+
+      <Button mode="contained" onPress={() => console.log('Pressed')}>
+        Press me
+      </Button>
     </View>
   )
 }
