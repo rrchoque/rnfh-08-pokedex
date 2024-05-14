31784e6db923c687eea360092ba27ff83576f018
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-05-14 08:18:49 -0400

    Se creó el procedimiento para obtener la información del pokemon en el PokemonScreem

--- src/presentation/screens/pokemon/PokemonScreen.tsx
+++ src/presentation/screens/pokemon/PokemonScreen.tsx
@@ -1,9 +1,28 @@
+import { StackScreenProps } from '@react-navigation/stack'
 import { View, Text } from 'react-native'
+import { RootStackParams } from '../../navigator/StackNavigator'
+import { useQuery } from '@tanstack/react-query';
+import { getPokemonById } from '../../../actions/pokemons';
 
-export const PokemonScreen = () => {
+interface Props extends StackScreenProps<RootStackParams, 'PokemonScreen'> {}
+
+export const PokemonScreen = ({navigation, route}: Props) => {
+
+  const {pokemonId} = route.params;
+
+  const {isLoading, data: pokemon} = useQuery({
+    queryKey: ['pokemon', pokemonId],
+    queryFn: () => getPokemonById(pokemonId),
+    staleTime: 1000 * 60 * 60, // 1 hour
+  });
+
+  if (!pokemon) {
+    return <Text>Cargando...</Text>;
+  }
+
   return (
     <View>
-      <Text>PokemonScreen</Text>
+      <Text>PokemonScreen { pokemon.name }</Text>
     </View>
   )
 }
