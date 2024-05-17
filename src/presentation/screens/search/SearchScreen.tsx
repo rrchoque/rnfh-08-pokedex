ad3222f8b768b865780bc47cfbca25ae5e400cc2
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-05-17 08:02:10 -0400

    Se creó el seachScreem y el listado de pokemons para buscar por nombre

--- src/presentation/screens/search/SearchScreen.tsx
+++ src/presentation/screens/search/SearchScreen.tsx
@@ -1,9 +1,45 @@
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
+import { globalTheme } from '../../../config/theme/global-theme'
+import { ActivityIndicator, Text, TextInput } from 'react-native-paper'
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { FlatList } from 'react-native-gesture-handler';
+import { PokemonCard } from '../../components';
+import { Pokemon } from '../../../domain/entities/pokemon';
+import { useQuery } from '@tanstack/react-query';
+import { getPokemonNamesWithId } from '../../../actions/pokemons';
 
 export const SearchScreen = () => {
+  const {top} = useSafeAreaInsets();
+
+  const {isLoading, data: pokemonNameList = []} = useQuery({
+    queryKey: ['pokemons', 'all'],
+    queryFn: () => getPokemonNamesWithId(),
+  });
+
+  console.log(pokemonNameList)
+
   return (
-    <View>
-      <Text>SearchScreen</Text>
+    <View style={[globalTheme.globalMargin, {paddingTop: top + 10, flex: 1}]}>
+      <TextInput
+        placeholder="Buscar Pokémon"
+        mode="flat"
+        autoFocus
+        autoCorrect={false}
+        onChangeText={value => console.log(value)}
+        value={'hola mundo'}
+      />
+
+      <ActivityIndicator style={{paddingTop: 20}} />
+
+      <FlatList
+        data={[] as Pokemon[]}
+        keyExtractor={(pokemon, index) => `${pokemon.id}-${index}`}
+        numColumns={2}
+        renderItem={({item: pokemon}) => <PokemonCard pokemon={pokemon} />}
+        style={{paddingTop: top + 20}}
+        showsVerticalScrollIndicator={false}
+      />
+
     </View>
   )
 }
